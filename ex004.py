c7abfcd30d651cac218871ce98eb56ce36f5de7f
melizamaia <melizamaia@gmail.com>
2023-09-04 00:27:39 -0300

    Atualizações de exercicios

--- ex004.py
+++ ex004.py
@@ -1,1 +1,11 @@
 #Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele
+
+teste = str(input("Digite algo no seu teclado: "))
+print("O tipo primitivo desse valor é ", type(teste))
+print("Só tem espaços? ", teste.isspace())
+print("É um numero? ", teste.isnumeric())
+print("É alfabético? ", teste.isalpha())
+print("É alfanumérico?", teste.isalnum())
+print("Está em maiúsculas? ", teste.isupper())
+print("Está em minúsculas? ", teste.islower())
+print("Está capitalizada? ", teste.istitle())

3015f43825dd223d7677b9ecc8441c7fb60d6c71
melizamaia <melizamaia@gmail.com>
2023-10-31 11:11:01 -0300

    Adicionando exercicios

---
+++ ex033.py
@@ -0,0 +1,1 @@
+## faça um programa que leia três números e mostre qual é o maior e qual é o menor
---
+++ ex035.py
@@ -0,0 +1,2 @@
+## Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou formar um triângulo
+## Exemplo: r1, r2 e r3

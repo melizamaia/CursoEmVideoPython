8e3c6981c8ce3172ca8d6e5d9eb5240b69bce659
melizamaia <melizamaia@gmail.com>
2023-11-04 16:12:04 -0300

    Adicionando exercicios

---
+++ ex036.py
@@ -0,0 +1,13 @@
+# nome = str(input('Qual é o seu nome? '))
+# if nome == "Meliza":
+#     print("Que nome bonito!")
+# elif nome == "Pedro" or nome == "Maria" or nome == "Ana":
+#     print("Seu nome é bem popular no Brasil")
+# elif nome in "Eliza Maria Lady":
+#     print("Belo nome feminino")
+# else:
+#     print("Seja bem vindo!")
+
+## Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
+## O programa vai perguntar o valor da casa, o salário do comprador e em quantos abos ele vai pagar.
+## Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.
---
+++ ex042.py
@@ -0,0 +1,4 @@
+## Refaça o desafio 035 dos triângulos, acrescentendo o recurso de mostrar que tipo de triângulo será formado:
+## Equilátero: todos os lados iguais
+## Isósceles: dois lados iguais
+## Escaleno: todos os lados diferentes

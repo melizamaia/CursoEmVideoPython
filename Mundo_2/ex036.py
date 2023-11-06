da8a4ab2c86731d19075fe65da1f4f02de24df02
melizamaia <melizamaia@gmail.com>
2023-11-06 19:36:50 -0300

    Adicionando exercicios

--- Mundo_2/ex036.py
+++ Mundo_2/ex036.py
@@ -1,13 +1,15 @@
-# nome = str(input('Qual é o seu nome? '))
-# if nome == "Meliza":
-#     print("Que nome bonito!")
-# elif nome == "Pedro" or nome == "Maria" or nome == "Ana":
-#     print("Seu nome é bem popular no Brasil")
-# elif nome in "Eliza Maria Lady":
-#     print("Belo nome feminino")
-# else:
-#     print("Seja bem vindo!")
-
 ## Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
-## O programa vai perguntar o valor da casa, o salário do comprador e em quantos abos ele vai pagar.
+## O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
 ## Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.
+
+casa = float(input("Valor da casa: R$ "))
+salario = float(input("Salário do comprador: R$ "))
+anos = int(input("Quantos anos de financiamento? "))
+prestacao = casa / (anos * 12)
+minimo = salario * 30 / 100
+print("Para pagar uma casa de R${:.2f} em {} anos".format(casa, anos), end="")
+print(" A prestação será de R${:.2f}".format(prestacao))
+if prestacao <= minimo:
+    print("Empréstimo pode ser concedido!")
+else:
+    print("Empréstimo pode ser negado!")

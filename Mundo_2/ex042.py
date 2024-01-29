09443827d0cb016f0433919cad0947166079fb95
melizamaia <melizamaia@gmail.com>
2024-01-29 14:30:16 -0300

    Exercicios 42 e 43

--- Mundo_2/ex042.py
+++ Mundo_2/ex042.py
@@ -2,3 +2,17 @@
 ## Equilátero: todos os lados iguais
 ## Isósceles: dois lados iguais
 ## Escaleno: todos os lados diferentes
+
+r1 = float(input("Primeiro segmento: "))
+r2 = float(input("Segundo segmento: "))
+r3 = float(input("Terceiro segmento: "))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print("Os segmentos acima PODEM FORMAR triângulo ", end='')
+    if r1 == r2 == r3:
+        print('EQUILATERO!')
+    elif r1 != r2 != r3 != r1:
+        print('ESCALENO!')
+    else:
+        print('ISOSCELES!')
+else:
+    print("Os segmentos acima NÃO PODEM FORMAR triângulo")
